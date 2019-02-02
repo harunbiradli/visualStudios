b1c1d0334394ad41414c7605a3d26fdbf36eecf8
Harun Biradlı <41165819+harunbiradli@users.noreply.github.com>
2019-02-02 18:33:44 +0300

    class yazımı

---
+++ resim işleme ve kaydetme 1.0/resim işleme ve kaydetme 1.0/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace resim_işleme_ve_kaydetme_1._0
+{
+
+    public class ImageProcessor
+    {
+        public ImageProcessor()
+        {
+ 
+        }
+        public ImageProcessor(Image parImage)
+        {
+            Img = parImage;
+        }
+ 
+        private Image _Img;
+        private Bitmap bmp;
+        public Image Img
+        {
+            get
+            {
+                return _Img;
+            }
+            set
+            {
+                _Img = value;
+                bmp = new Bitmap(_Img);
+            }
+        }
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
